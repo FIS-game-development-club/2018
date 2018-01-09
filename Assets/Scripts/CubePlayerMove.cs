6a3c339098e19cc06bf2fbc5582adf2227cdeacf
Frederik Hollis <frederik_hollis@fis.edu>
2018-01-09 17:14:29 +0100

    move_script_W_jumpforward

---
+++ Assets/Scripts/CubePlayerMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			gameObject.transform.Translate(Vector3.forward);
+		}
+	}
+}
