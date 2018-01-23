b0547f2622f475e1cad0953df80d4482eb9b12d3
karsten2004 <karsten_vanderdeijl@fis.edu>
2018-01-23 17:53:57 +0100

    added test level and fixed script

--- Assets/Scripts/Pausemenu.cs
+++ Assets/Scripts/Pausemenu.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pausemenu : MonoBehaviour {
+	public static bool paused = false;
+	public GameObject PauseMenu;
 
-	// Use this for initialization
-	void Start () {
-		
+	public void resume(){
+		PauseMenu.SetActive (false);
+		Time.timeScale = 1;
+		paused = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+	void pause (){
+		PauseMenu.SetActive (true);
+		Time.timeScale = 0;
+		paused = true;
+	}
+	void Start () {
+		PauseMenu.SetActive (false);
+	}
 		
+		void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if(paused){
+				resume ();
+			}
+			else{
+				pause();
+			}
+		}
 	}
 }
