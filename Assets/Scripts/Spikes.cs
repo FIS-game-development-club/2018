a3a1648102c455fe5c32d1a0089201b0feb0b3e2
karsten2004 <karsten_vanderdeijl@fis.edu>
2018-01-30 18:13:53 +0100

    added script

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour 
+{
+    Pausemenu Pausemenu = GameObject.Find("SceneManager").GetComponent<Pausemenu> ();
+
+    void OnCollisionEnter(Collision collide)
+	{
+        if (collide.gameObject.name == "CubePlayer") {
+			Debug.Log ("Player died");
+			Pausemenu.pause ();
+		}
+	
+	}
+}
