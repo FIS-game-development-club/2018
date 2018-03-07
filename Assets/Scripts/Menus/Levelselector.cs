0fbc9d880071615dc3a36053ca7ae65dc4865f2c
karsten2004 <karsten_vanderdeijl@fis.edu>
2018-03-07 20:22:02 +0100

    added high scores

---
+++ Assets/Scripts/Menus/Levelselector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Levelselector : MonoBehaviour {
+
+    public string level;
+    public int levelnumber;
+    public Text highscore;
+    int Highscore;
+
+    void Update(){
+        Highscore = PlayerPrefs.GetInt("highscore " + levelnumber.ToString(), 0);
+        highscore.text = "Highscore: " + Highscore.ToString();
+    }
+	public void to_level () {
+        if (PlayerPrefs.GetInt("Levelnumber", 1) >= levelnumber)
+        {
+            SceneManager.LoadScene(level);
+        }
+	}
+}
