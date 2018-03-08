d16eba5c4c7950bd06900ba6117b7e8f920fe243
karsten2004 <karsten_vanderdeijl@fis.edu>
2018-03-08 19:17:02 +0100

    d

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour {
     public string scene;
     public int Levelnumber;
+    public GameObject scenemanager;
     void OnTriggerEnter(Collider c)
     {
         if (c.gameObject.name == "CubePlayer")
         {
+            if()
             SceneManager.LoadScene(scene);
             if (PlayerPrefs.GetInt("Levelnumber", 1) >= Levelnumber){
                 PlayerPrefs.SetInt("Levelnumber", Levelnumber + 1);
