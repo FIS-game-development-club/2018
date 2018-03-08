c9b31e2beadb5eb3ebc08aaa71242b1c3e85a9a7
karsten2004 <karsten_vanderdeijl@fis.edu>
2018-03-08 19:09:17 +0100

    d

--- Assets/Scripts/Menus/Highscore.cs
+++ Assets/Scripts/Menus/Highscore.cs
@@ -7,12 +7,13 @@ public class Highscore : MonoBehaviour {
     public int levelnumber;
     public Text highscore;
     int highscoreint;
-    public Text time; 
+    public Text time;
+    public int score;
 
     void FixedUpdate () {
         highscoreint = PlayerPrefs.GetInt("highscore " + levelnumber.ToString(), 0);
         highscore.text = "Highscore: " + highscoreint.ToString();
-        time.text = Time.deltaTime.ToString();
-
+        score = (int)Time.deltaTime;
+        time.text = score.ToString();
 	}
 }
