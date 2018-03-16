959e7de3d710698756ef3a7016e94a1b90980403
mvpwizard <mvportiera@gmail.com>
2018-03-16 13:23:41 +0100

    modifications

--- Assets/Scripts/BindPlayer.cs
+++ Assets/Scripts/BindPlayer.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class BindPlayer : MonoBehaviour 
 {
+	Vector3 l;
+	Vector3 v;
+
+	void FixedUpdate()
+	{
+		Vector3 n = gameObject.transform.position;
+		v = n - l;
+		l = n;
+	}
 
 	void OnTriggerEnter(Collider c)
 	{
 		GameObject g = c.gameObject;
 		if (g.name == "CubePlayer")
 		{
+			if (removed.Contains(g))
+				removed.Remove(g);
 			g.transform.SetParent(transform);
 		}
+	}
+
+	List<GameObject> removed = new List<GameObject>();
+	void OnTriggerExit(Collider c)
+	{
+		GameObject g = c.gameObject;
+		if (g.name == "CubePlayer")
+		{
+			removed.Add(g);
+		}
+	}
+
+	public void clear()
+	{
+		foreach (GameObject g in removed)
+		{
+			if (g.transform.parent == transform)
+				g.transform.SetParent(null);
+		}
+		removed.Clear();
 	}
 }
