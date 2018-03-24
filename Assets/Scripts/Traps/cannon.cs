c093465d806ee21b488438c35e10cdb3f36f5469
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-03-24 12:25:08 +0100

    added in cannon
    - made cannon model and added it into unity
    - made cannon track player at all time

---
+++ Assets/Scripts/Traps/cannon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannon : MonoBehaviour {
+
+    GameObject player;
+    Transform playerP;
+
+    void Start () {
+        player = GameObject.Find("CubePlayer");
+        playerP = player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(playerP);
+	}
+}
