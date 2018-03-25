00e8e93cb12e66d86027a81e776d7d4d8fce9975
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-03-25 08:48:02 +0200

    done
    added script
    added texture
    it works

---
+++ Assets/Scripts/Traps/fireballs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireballs : MonoBehaviour {
+
+    Deathmenu deathmenu;
+
+ 	void Start () {
+        
+        deathmenu = GameObject.Find("SceneManager").GetComponent<Deathmenu>();
+	}
+
+	private void OnParticleCollision(GameObject collide)
+	{
+        { 
+            if (collide.name == "CubePlayer")
+            {
+                deathmenu.died();
+            }
+        }
+	}
+}
