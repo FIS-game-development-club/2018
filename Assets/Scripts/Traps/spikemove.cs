019bab7ea107b7eaa715de2b8f65bbb03be4a92d
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-04-11 17:17:18 +0200

    added new moving spikes and 3x3 moving spikes prefabs

--- Assets/Scripts/Traps/spikemove.cs
+++ Assets/Scripts/Traps/spikemove.cs
@@ -6,13 +6,19 @@ public class spikemove : MonoBehaviour
 {
 	public float time_up;
 	public float time_down;
-	public GameObject up_position;
-	public GameObject down_position;
+	public GameObject position1;
+	public GameObject position2;
 	public float shift_speed;
-
 	float time_elapsed;
-	public bool isUp;
+	public bool group;
+	bool isUp;
 
+	void Start(){
+		position1.transform.position = gameObject.transform.position;
+		if (group){
+			
+		}
+	}
 	void Update () 
 	{
 		//keep track of time
@@ -39,16 +45,16 @@ public class spikemove : MonoBehaviour
 		if (isUp) {
 			gameObject.transform.position = 
 				Vector3.Lerp (
-					down_position.transform.position,
-					up_position.transform.position,
+					position2.transform.position,
+					position1.transform.position,
 					1 / shift_speed * time_elapsed);
 		} 
 		else 
 		{
 			gameObject.transform.position = 
 				Vector3.Lerp (
-					up_position.transform.position,
-					down_position.transform.position,
+					position1.transform.position,
+					position2.transform.position,
 					1 / shift_speed * time_elapsed);
 		}
 	}
