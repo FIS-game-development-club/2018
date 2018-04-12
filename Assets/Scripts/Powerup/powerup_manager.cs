8b9d90dabc31b2e381763c51ae9d21fc836f0c08
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-04-12 21:03:36 +0200

    being worked on

---
+++ Assets/Scripts/Powerup/powerup_manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerup_manager : MonoBehaviour {
+
+	Deathmenu deathmenu;
+	GameObject[] powerups;
+	void Start () {
+		deathmenu = GameObject.FindWithTag("Scenemanager").GetComponent<Deathmenu>();
+	}
+
+	void Update () {
+		if (deathmenu.active){
+			foreach(GameObject powerup in powerups){
+				powerup.SetActive(true);
+			}
+		}
+	}
+}
