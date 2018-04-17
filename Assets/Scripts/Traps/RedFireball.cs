3e128a28baeba060591853508a3bc0ae3c4ba3ae
mvpwizard <mvportiera@gmail.com>
2018-04-17 11:27:10 +0200

    Fireballs Added

---
+++ Assets/Scripts/Traps/RedFireball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedFireball : MonoBehaviour 
+{
+	public float speed;
+	public float duration;
+	float lifetime;
+	ParticleSystem ps;
+	float fulllife;
+	void Start () 
+	{
+		ps = gameObject.GetComponent<ParticleSystem>();
+		fulllife = ps.startLifetime;
+	}
+	
+	void Update () 
+	{
+		if (lifetime >= duration)
+			GameObject.Destroy(gameObject);
+		else
+		{
+			lifetime += Time.deltaTime;
+			ps.startLifetime = fulllife * (1 - (lifetime / duration));
+			transform.Translate(gameObject.transform.forward * speed * Time.deltaTime);
+		}
+		
+	}
+}
