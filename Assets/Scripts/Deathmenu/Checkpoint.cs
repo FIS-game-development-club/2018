13bb67cf3a225afa8b0cd9895b9d9dac709ca024
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-05-03 16:11:10 +0200

    m

--- Assets/Scripts/Deathmenu/Checkpoint.cs
+++ Assets/Scripts/Deathmenu/Checkpoint.cs
@@ -26,7 +26,7 @@ public class Checkpoint : MonoBehaviour
                 usable = false;
                 checkpointGot = true;
                 
-1            }
+            }
         }
     }
 }
